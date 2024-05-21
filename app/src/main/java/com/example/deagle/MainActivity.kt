5c14cfc216b3fafc6b4ea143aa2d6eb7834d19c9
Eldis Simone <sayapakailinuxpak@gmail.com>
2024-05-21 15:20:19 +0700

    fix action bar not showing correctly

--- app/src/main/java/com/example/deagle/MainActivity.kt
+++ app/src/main/java/com/example/deagle/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_class, R.id.navigation_transaksi,R.id.navigation_profile,
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
